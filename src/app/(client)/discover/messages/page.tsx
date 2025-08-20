0c0e87ca0200135b404b7f937d1a2a8552373455
Rathan H N <rathancoorg11@gmail.com>
2025-08-20 17:24:27 +0000

    Remove that Your Conversations Header and stick the left panel that lis

--- src/app/(client)/discover/messages/page.tsx
+++ src/app/(client)/discover/messages/page.tsx
@@ -4,9 +4,6 @@ import ChatInterface from '@/components/chat-interface';
 export default function ClientMessagesPage() {
   return (
     <div className="flex flex-col h-[calc(100vh-64px)]">
-        <div className="container mx-auto py-4">
-            <h1 className="text-2xl font-bold">Your Conversations</h1>
-        </div>
         <div className="flex-1 border-t">
              <ChatInterface />
         </div>
